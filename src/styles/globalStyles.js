28d2f2d928d7822fb0be1824c46267d40e39f7de
gabrielalexsander18 <gabrielfaria257@gmail.com>
2024-01-22 14:14:16 -0300

    Adicionado estilização na scrollbar

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -10,4 +10,42 @@ export default createGlobalStyle`
     font-family: 'Montserrat', sans-serif;
     outline: none;
  }
+
+ &::-webkit-scrollbar {
+    width: 9px;
+  }
+  /* &::-webkit-scrollbar-button {
+    width: 10px;
+    border-radius: 0 0 2px 2px;
+    background: #ff0023;
+  } */
+  &::-webkit-scrollbar-track {
+
+    background-color: #0a0a0a;
+  }
+  /* &::-webkit-scrollbar-track-piece {
+    width: 100px;
+    border-radius: 30px;
+    background: #ff0023;
+  } */
+  &::-webkit-scrollbar-thumb {
+    border-radius: 30px;
+    background: #C0C0C0;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgba(192,192,192, .7);
+  }
+
+  &::-webkit-scrollbar-thumb:active {
+    background: rgba(192,192,192, .9);
+  }
+  /* &::-webkit-scrollbar-corner {
+    width: 100px;
+    color: #ff0023;
+  } */
+  /* &::-webkit-resizer {
+    width: 100px;
+    background: #ff0023;
+  } */
 `
